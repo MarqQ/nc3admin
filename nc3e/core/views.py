5ec46bf5a70b4a97efc7e9dd7d940e76a4a2b8c6
MarcQ <marcos.w.queiroz@gmail.com>
2020-06-01 12:21:29 -0300

    Importando templates para o projeto admin - pequenas correções de nomenclatura nos templates

--- nc3e/core/views.py
+++ nc3e/core/views.py
@@ -9,28 +9,52 @@ def index(request):
     return render(request, 'index.html')
 
 
+def clientes(request):
+    return render(request, 'clientes/clientes.html')
+
+
+def clientesAdd(request):
+    return render(request, 'clientes/clientes-add.html')
+
+
+def clientesEdit(request):
+    return render(request, 'clientes/clientes-edit.html')
+
+
+def fornecedores(request):
+    return render(request, 'fornecedores/fornecedores.html')
+
+
+def fornecedoresAdd(request):
+    return render(request, 'fornecedores/fornecedores-add.html')
+
+
+def fornecedoresEdit(request):
+    return render(request, 'fornecedores/fornecedores-edit.html')
+
+
 def obras(request):
-    return render(request, 'obras.html')
+    return render(request, 'obras/obras.html')
 
 
 def obrasAdd(request):
-    return render(request, 'obrasAdd.html')
+    return render(request, 'obras/obras-add.html')
 
 
 def obrasEdit(request):
-    return render(request, 'obrasEdit.html')
+    return render(request, 'obras/obras-edit.html')
 
 
 def usuarios(request):
-    return render(request, 'usuarios.html')
+    return render(request, 'usuarios/usuarios.html')
 
 
 def usuariosAdd(request):
-    return render(request, 'usuarios-add.html')
+    return render(request, 'usuarios/usuarios-add.html')
 
 
 def usuariosEdit(request):
-    return render(request, 'usuarios-edit.html')
+    return render(request, 'usuarios/usuarios-edit.html')
 
 
 def index2(request):
