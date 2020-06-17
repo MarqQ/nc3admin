da7aa1ea55768f3d96e94702ce49b03e6ee3feea
MarqQ <marcos.w.queiroz@gmail.com>
2020-06-17 13:43:01 -0300

    Ajuste de identacao

--- nc3e/core/models.py
+++ nc3e/core/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     cpf = models.CharField(max_length=11)
     cnpj = models.CharField(max_length=14)
     data_nascimento = models.DateField
-    telefone = models.CharField(max_length=10)
+    telefone = models.CharField(max_length=11)
     cep = models.CharField(max_length=8)
     estado = models.CharField
     cidade = models.CharField(max_length=20)
